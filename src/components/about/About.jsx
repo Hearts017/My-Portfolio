c13e654ae1d0c1188f0106eb8a75b2573a36d2a7
Jommel Toledo <jommeltoledo17@gmail.com>
2023-07-16 03:11:10 +0800

    replacing pdf file

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/My-CV.pdf";
+import CV from "../../assets/JommelToledo.pdf";
 import Info from "./Info";
 
 const About = () => {
